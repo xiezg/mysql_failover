feef3e5703d015f749350c20503e99f9cbcdd88e
xiezg <zgxie@aibee.com>
2023-04-26 14:54:06 +0800

    修改Dockerfile

--- test/build.sh
+++ test/build.sh
@@ -3,7 +3,7 @@
 # Author: xiezg
 # mail: xzghyd2008@hotmail.com
 # Created Time: 2023-03-14 19:58:23
-# Last modified: 2023-04-03 18:37:38
+# Last modified: 2023-04-26 11:28:22
 #########################################################################
 #!/bin/bash
 
@@ -12,6 +12,7 @@
 
 image_name=harbor.aibee.cn/platform/mysql-failover:v0.0.1
 
-docker build -t $image_name .
+#docker build  --no-cache -t $image_name -f ./Dockerfile  ..
+docker build  -t $image_name -f ./Dockerfile  ..
 docker push $image_name
 
